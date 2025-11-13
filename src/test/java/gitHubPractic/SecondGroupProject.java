1de99bae6ce10c6816715017249d5bde25c65eab
aisuluuomokoeva <aomokoeva@gmail.com>
2025-11-12 19:54:11 -0600

    First commite

---
+++ src/test/java/gitHubPractic/SecondGroupProject.java
@@ -0,0 +1,11 @@
+package gitHubPractic;
+
+public class SecondGroupProject {
+
+    public static void main(String[] args) {
+        System.out.println("First change");
+        System.out.println("First change");
+        System.out.println("First change");
+        System.out.println("First change");
+    }
+}
