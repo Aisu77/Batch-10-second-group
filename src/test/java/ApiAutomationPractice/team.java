fb9ae016e0872f49f33437a348e810b6b0384658
TarasOre <romskynazar@gmail.com>
2025-11-12 20:42:19 -0600

    I create new class team

---
+++ src/test/java/ApiAutomationPractice/team.java
@@ -0,0 +1,11 @@
+package ApiAutomationPractice;
+
+public class team {
+    public static void main(String[] args) {
+        System.out.println("I create some lines");
+        System.out.println("I create some lines");
+        System.out.println("I create some lines");
+        System.out.println("I create some lines");
+        System.out.println("I create some lines");
+    }
+}
