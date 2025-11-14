d928566faebc5ab086d12115d7e47214b6ac93b1
TarasOre <romskynazar@gmail.com>
2025-11-13 19:52:21 -0600

    Conflict class hello

--- src/test/java/practice_conflicts/Hello.java
+++ src/test/java/practice_conflicts/Hello.java
@@ -21,6 +21,7 @@ public class Hello {
         System.out.println("Hello we will practice git conflicts here");
         System.out.println("Hello we will practice git conflicts here");
         System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
         int a = 10;
         int b = 20;
         System.out.println(a + b);
