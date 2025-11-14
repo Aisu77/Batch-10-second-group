be6fa64226f2288c750c8def557b066908dd6aac
SULTAN <sultan.omorbekov01@gmail.com>
2025-11-13 19:27:03 -0600

    sum of two num

--- src/test/java/practice_conflicts/Hello.java
+++ src/test/java/practice_conflicts/Hello.java
@@ -16,5 +16,8 @@ public class Hello {
         System.out.println("Hello we will practice git conflicts here");
         System.out.println("Hello we will practice git conflicts here");
         System.out.println("Hello we will practice git conflicts here");
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
     }
 }
