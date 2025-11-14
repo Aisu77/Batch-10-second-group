2fd3997f06c95ea5e220a455c9db9b8826521aa2
codewise-nurbek <nabdirasulov0@gmail.com>
2025-11-13 18:25:53 -0600

    added class for practicing git conflicts

---
+++ src/test/java/practice_conflicts/Hello.java
@@ -0,0 +1,20 @@
+package practice_conflicts;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts here");
+    }
+}
