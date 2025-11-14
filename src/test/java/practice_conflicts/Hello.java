47c9cb322f5a7f91780d1995ee9a07f37fb9d22b
aisuluuomokoeva <aomokoeva@gmail.com>
2025-11-13 19:27:44 -0600

    This is Aisuluus commit

--- src/test/java/practice_conflicts/Hello.java
+++ src/test/java/practice_conflicts/Hello.java
@@ -1,20 +1,15 @@
 package practice_conflicts;
 
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
+
+        for(int i = 0;i < 10;i++){
+            System.out.println("This is Aisuluu's change N" + (i + 1));
+            System.out.println("This is Aisuluu's change");
+            System.out.println("This is Aisuluu's change");
+            System.out.println("This is Aisuluu's change");
+        }
     }
 }
