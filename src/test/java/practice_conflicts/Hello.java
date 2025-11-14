9f84a5dac7b1eb38280f20ba2dea091b7475a45a
TarasOre <romskynazar@gmail.com>
2025-11-13 19:36:43 -0600

    Conflict class hello

--- src/test/java/practice_conflicts/Hello.java
+++ src/test/java/practice_conflicts/Hello.java
@@ -2,19 +2,8 @@ package practice_conflicts;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
+        System.out.println("Hello we will practice git conflicts ");
+        System.out.println("Hello we will practice git conflicts ");
+        System.out.println("conflict");
     }
 }
