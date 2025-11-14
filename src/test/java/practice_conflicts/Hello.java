67074df50a5ac4282b72ad7454c8f56620200a34
elzuura <sultanovaelya75@gmail.com>
2025-11-13 17:38:29 -0800

    new commit

--- src/test/java/practice_conflicts/Hello.java
+++ src/test/java/practice_conflicts/Hello.java
@@ -2,19 +2,11 @@ package practice_conflicts;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
-        System.out.println("Hello we will practice git conflicts here");
+        for (int i = 0; i < 11; i++) {
+
+            System.out.println("Hello we will practice git conflicts here");
+
+        }
+        System.out.println("Here is optimized code");
     }
 }
