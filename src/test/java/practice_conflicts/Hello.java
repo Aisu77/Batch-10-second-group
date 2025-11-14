59d30a9098f7cb9cbd0875a298757008f40d6e91
ariunalapointe <ariuna.lapointe@gmail.com>
2025-11-13 20:34:34 -0500

    add more logic

--- src/test/java/practice_conflicts/Hello.java
+++ src/test/java/practice_conflicts/Hello.java
@@ -1,7 +1,8 @@
 package practice_conflicts;
 
-public class Hello {
+public class git branHello {
     public static void main(String[] args) {
+        System.out.println("Resolving conflict for the branch");
         System.out.println("Hello we will practice git conflicts here");
         System.out.println("Hello we will practice git conflicts here");
         System.out.println("Hello we will practice git conflicts here");
